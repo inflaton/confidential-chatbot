6be256a409f6f011577b7897f6ffbb4a15312e68
Donghao Huang <donghao.huang@mastercard.com>
2023-05-25 12:40:47 +0800

    outpu CUDA version in test

--- server/test.py
+++ server/test.py
@@ -23,9 +23,12 @@ print()
 # Additional Info when using cuda
 if device.type == "cuda":
     print(torch.cuda.get_device_name(0))
+    print("CUDA version: " + torch.version.cuda)
+    
     print("Memory Usage:")
     print("Allocated:", round(torch.cuda.memory_allocated(0) / 1024**3, 1), "GB")
     print("Cached:   ", round(torch.cuda.memory_reserved(0) / 1024**3, 1), "GB")
+    
 
 # Constants
 load_dotenv(override=True)
