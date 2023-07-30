77147a7e93a5e0ded7f0b474f0d637d8afef9f0e
Donghao Huang <Donghao.Huang@mastercard.com>
2023-07-30 16:14:57 +0800

    fix cors headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 const getCorsHeaders = () => {
   const headers = {};
 
-  headers["Access-Control-Allow-Origin"] = "http://localhost:1420";
+  headers["Access-Control-Allow-Origin"] = "https://confidential-chatbot.glitch.me";
   headers["Access-Control-Allow-Credentials"] = "true";
   headers["Access-Control-Allow-Methods"] =
     "GET,OPTIONS,PATCH,DELETE,POST,PUT";
